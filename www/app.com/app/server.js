f383fc3fa2a825f08e2f056625454bed7dbfee57
Bradley Griffiths <bradley.griffiths@gmail.com>
2011-11-27 20:01:52 +0000

    removed npm modules

--- www/app.com/app/server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Yes! successfully added. Commit changes to update you application.');
-  res.end();
-}).listen(process.env.PORT || 9001);
