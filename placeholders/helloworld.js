ba98b981e809a227d2313763e89e3b977bcd7bf4
bradley griffiths <bradley.griffiths@gmail.com>
2011-11-29 11:30:01 +0000

    error checking

--- placeholders/helloworld.js
+++ placeholders/helloworld.js
@@ -3,4 +3,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('Yes! successfully added. Commit changes to update you application.');
   res.end();
-}).listen(process.env.PORT || 9001);
+}).listen(process.env.PORT);
